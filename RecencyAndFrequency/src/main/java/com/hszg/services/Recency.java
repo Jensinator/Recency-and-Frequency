40d27e5df867eb5f9159e98163ae3ff1a0433c13
Jens Leuschner <s3jeleus@stud.hszg.de>
2015-09-17 11:14:37 +0200

    Replace external lib folder.

--- RecencyAndFrequency/src/main/java/com/hszg/services/Recency.java
+++ RecencyAndFrequency/src/main/java/com/hszg/services/Recency.java
@@ -1,6 +1,8 @@
 package com.hszg.services;
 
 import com.hszg.dto.Point;
+import com.kianaanalytics.computeNode.dataaccess.PersonVisibleAccessService;
+import com.kianaanalytics.computeNode.model.PersonVisibleResult;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -22,6 +24,12 @@ public class Recency {
 	@Path("/RecencyDistribution")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Point getRecencyDistribution(){
+		
+		PersonVisibleAccessService observationService = new PersonVisibleAccessService();
+		
+		//PersonVisibleResult Observations = observationService.computePersonVisible();
+		
+		//observationService.showResults( Observations.getResultCollection() );
 		
 		return new Point(1.0f,13.0f);
 	}
