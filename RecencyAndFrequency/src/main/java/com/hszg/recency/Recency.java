fd54601249b6ecc69d5b30f3aa89052b21959529
Jens Leuschner <s3jeleus@stud.hszg.de>
2015-09-24 15:40:17 +0200

    Implement procedure to compute the recency distribution for a client mac.

--- RecencyAndFrequency/src/main/java/com/hszg/recency/Recency.java
+++ RecencyAndFrequency/src/main/java/com/hszg/recency/Recency.java
@@ -5,10 +5,12 @@ package com.hszg.recency;
 import com.hszg.dto.RecencyDistribution;
 import com.kianaanalytics.computeNode.model.ObservationOfClientMac;
 import com.kianaanalytics.computeNode.model.ObservationsOfClientMac;
+import com.kianaanalytics.computeNode.model.PartialObservation;
 import com.kianaanalytics.computeNode.model.PersonVisibleResult;
 import com.kianaanalytics.computeNode.util.BigQueryDate;
 
 public class Recency {
+	
 	
 	/** This function computes the recency for a specific client mac.
 	 *	@param clientMac - The client mac the recency should be computed for.
@@ -25,39 +27,44 @@ public class Recency {
 		
 		RecencyDistribution distribution = new RecencyDistribution();
 		
-		for( int i = 0; i < observationsForClientMac.size(); i++ ){
+		// elementary event
+		if(observationsForClientMac.size() < 2 ){
+			distribution.addRecencyValue(RecencyDistribution.NO_RECENCY);
+		}
+		
+		// normal event
+		for( int i = 0; i < observationsForClientMac.size() - 1; i++ ){
 			
-			ObservationOfClientMac observation = observationsForClientMac.get(i);
+			ObservationOfClientMac observation1 = observationsForClientMac.get(i);
+			ObservationOfClientMac observation2 = observationsForClientMac.get(i+1);
 			
-			// elemental case
-			if(observation.keySet().size() < 2 ){
-				distribution.addRecencyValue(RecencyDistribution.NO_RECENCY);
-			}
-
+			int recency = computeRecency(observation1, observation2);
 			
+			distribution.addRecencyValue(recency);
 			
-			// else case
-			for( int j = 0; j < observation.keySet().size() - 1; j++ ){
-				
-				long keyEndTime   = (long)observation.keySet().toArray()[j];
-				long keyStartTime = (long)observation.keySet().toArray()[j+1];
-				
-				//Time
-				BigQueryDate endTime = observation.get(keyEndTime).getEndObservationTimeUtc();
-				BigQueryDate startTime = observation.get(keyStartTime).getStartObservationTimeUtc();	
-				
-				System.out.println("end t : " + endTime );
-				System.out.println("start t : " + startTime);
-				
-				int recency = DateCalculator.getDiffInDays(endTime, startTime);
-				
-				distribution.addRecencyValue(recency);
-			}
-
 		}
 		
 		return distribution;
 	}
 	
+	
+	/** This function computes the difference in days between the end time of the first and the start time of
+	 *  the second observation.
+	 *  @param observation1 - first observation in time to take end time from.
+	 *  @param observation2 - second observation in time to take start time from.
+	 *  @return The recency.
+	 **/
+	private static int computeRecency( ObservationOfClientMac observation1, ObservationOfClientMac observation2 ){
+		
+		PartialObservation partObservation1 = observation1.firstEntry().getValue();
+		PartialObservation partObservation2 = observation2.firstEntry().getValue();
+		
+		BigQueryDate endTime   = partObservation1.getEndObservationTimeUtc();
+		BigQueryDate startTime = partObservation2.getStartObservationTimeUtc();	
+		
+		int recency = DateCalculator.getDiffInDays(endTime, startTime);
+		
+		return recency;
+	}
 	
 }
